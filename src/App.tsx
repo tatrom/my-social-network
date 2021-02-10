1d28eb262fc77d08ae1e06eda8f3cc0afbda461b
roman <roman.gadiev@gmail.com>
2021-02-10 08:53:45 +0200

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,31 @@ import './App.css';
 import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
-import {Route} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Switch, Redirect} from 'react-router-dom';
 import {DialogsContainer} from "./Components/Dialogs/DialogsContainer";
 import {UserContainer} from "./Components/Users/UsersContainer";
 
 
-
-
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Navbar/>
-            <div className={"App_content"}>
-                <Route path="/profile" render={() => <Profile />}/>
-                <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                <Route path="/news"/>
-                <Route path="/music"/>
-                <Route path="/settings"/>
-                <Route path="/users" render={() => <UserContainer />} />
-            </div>
-        </div>
+        <BrowserRouter>
+            <div className="App">
 
+                <Header/>
+                <Navbar/>
+                <div className={"App_content"}>
+                    <Switch>
+                        <Route path={"/"} exact render={() => <Redirect to={'/profile'}/>}/>
+                        <Route path="/profile" render={() => <Profile/>}/>
+                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                        <Route path="/news"/>
+                        <Route path="/music"/>
+                        <Route path="/settings"/>
+                        <Route path="/users" render={() => <UserContainer/>}/>
+                    </Switch>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
